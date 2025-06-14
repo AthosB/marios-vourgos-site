f3482bb7df0c125f49a75d96810a78957292de65
Athos <a.boudourakis@gmail.com>
2025-06-15 01:09:27 +0300

    feat(*): Base - add submenu for Gallery menuitem hover - add styles to support he above

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,12 +1,32 @@
+"use client"
+
+import React, {useState} from "react";
 import '@/styles/mario.scss';
 import Image from "next/image";
 import {MsearchField} from "@/components/UI/MsearchField";
 import InputAdornment from '@mui/material/InputAdornment';
 import FormControl from '@mui/material/FormControl';
 import SearchIcon from '@mui/icons-material/Search';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
 
 export default function NavBar() {
+  /** HOOKS **/
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
   /** CONSTS **/
+  const open = Boolean(anchorEl);
+
+  const galleryMenuHover = (event: React.MouseEvent<HTMLDivElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  /**
+	 * Handles the close event to close the menu.
+	 */
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
 
 
   /** RENDER **/
@@ -20,18 +40,45 @@ export default function NavBar() {
         <div className={'entry'}>Home</div>
         <div className={'entry'}>About</div>
         <div className={'entry'}>News</div>
-        <div className={'entry'}>Gallery</div>
+        <div
+          style={{display: 'inline-block'}}
+          onMouseEnter={galleryMenuHover}
+          onMouseLeave={handleClose}
+        >
+          <div
+            className={`entry${open ? ' active' : ''}`}
+            id="gallery-menu-item"
+          >
+            Gallery
+          </div>
+          <Menu
+            id="basic-menu"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            slotProps={{
+              list: {
+                'aria-labelledby': 'basic-button',
+              },
+            }}
+            className={"PopupMenu"}
+          >
+            <MenuItem onClick={handleClose}>Photography</MenuItem>
+            <MenuItem onClick={handleClose}>Paintings</MenuItem>
+            <MenuItem onClick={handleClose}>Literature</MenuItem>
+          </Menu>
+        </div>
         <div className={'entry'}>Contact</div>
       </div>
       <div>
         <FormControl variant="filled">
           <MsearchField
-            id="input-with-icon-adornment"
+            id="search-field-icon-adornment"
             variant={'filled'}
             slotProps={{
               input: {
                 startAdornment: <InputAdornment position="start"><InputAdornment position="start">
-                  <SearchIcon htmlColor={'var(--mario-bg-color)'} color={'var(--mario-bg-color)'} />
+                  <SearchIcon htmlColor={'var(--mario-bg-color)'} />
                 </InputAdornment></InputAdornment>,
               },
             }}
