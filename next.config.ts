25770991be5c554b91a4a64aaa5539ccdd16491d
Athos <a.boudourakis@gmail.com>
2025-12-20 13:20:38 +0200

    feat(*): Base - NextJS - add headers video cache control for Next configuration - change version to 0.12.7

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,36 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async headers() {
+    return [
+      {
+        source: "/video/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = {
   images: { unoptimized: true },
+  async headers() {
+    return [
+      {
+        source: "/video/:path*",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "public, max-age=31536000, immutable",
+          },
+        ],
+      },
+    ];
+  },
   async redirects() {
     return [
       {
